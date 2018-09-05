7046303e5204943058f0c0b8e096ca022d695c16
Agustin Bala <agustin.bala@globallogic.com>
2018-09-05 12:33:26 -0300

    added companies logos

--- app/src/main/java/com/unidadcoronaria/doctorencasa/util/ProviderUtil.java
+++ app/src/main/java/com/unidadcoronaria/doctorencasa/util/ProviderUtil.java
@@ -9,8 +9,9 @@ public class ProviderUtil {
         switch (SessionUtil.getProvider()) {
             case 1:  tel += "1142577777"; break;
             case 2:  tel += "1142240202"; break;
-            case 3:  tel += "08102227100"; break;
+            case 5:  tel += "08102227100"; break;
             case 4:  tel += "1148607000"; break;
+            case 3:  tel += "1142243600"; break;
             default:
                 tel += "1142577777"; break;
         }
@@ -20,10 +21,11 @@ public class ProviderUtil {
 
     public static int getIcon(int id){
         switch (id) {
-            case 1: return R.drawable.ic_company_logo;
-            case 2: return R.drawable.ic_company_logo;
-            case 3: return R.drawable.ic_company_logo;
-            case 4: return R.drawable.ic_company_logo;
+            case 1: return R.drawable.logo_unidad_coronaria;
+            case 2: return R.drawable.logo_emeca_salud;
+            case 3: return R.drawable.logo_ayuda_medica;
+            case 4: return R.drawable.logo_sume_salud;
+            case 5: return R.drawable.logo_ayuda_medica;
             default:
                 return  R.drawable.ic_company_logo;
         }
